f14084afa870c2bf772d53e75e54efbf1689738d
Kasheesh <kasheesh171995@gmail.com>
2023-02-18 16:05:41 +0100

    PERF: REFACTORED details page to use Layout component

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,30 +1,14 @@
 import { Grid, makeStyles, createStyles } from "@material-ui/core";
 import { useContext } from "react";
 import { MoviesContext } from "../../App";
+import Layout from "../../components/Layout";
 import MovieDetailsList from "../../components/Lists/MovieDetailsList";
-import Navigation from "../../components/Navigation";
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    container: {
-      minHeight: "100vh",
-      width: "100vw",
-      justifyContent: "space-evenly",
-      background: "#F6F9FB",
-      padding: theme.spacing(1),
-    },
-    buttonContainer: {
-      margin: theme.spacing(5),
-    },
-  })
-);
+import { Pages } from "../../utils";
 
 export default function DetailsPage() {
-  const classes = useStyles();
   const { selectedMovie } = useContext(MoviesContext);
   return (
-    <Grid container className={classes.container}>
-      <Navigation />
+    <Layout page={Pages.MOVIE}>
       <Grid container justifyContent="space-evenly">
         <Grid item xs={6}>
           <img
@@ -37,6 +21,6 @@ export default function DetailsPage() {
           <MovieDetailsList />
         </Grid>
       </Grid>
-    </Grid>
+    </Layout>
   );
 }
